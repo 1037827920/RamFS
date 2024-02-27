384a558598ca41c173ff8be9304cde04cd99ed37
1037827920 <114841534+1037827920@users.noreply.github.com>
2024-02-27 11:02:58 +0800

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,3 +1,32 @@
+extern crate libc;
+use libc::mount;
+use libc::c_char;
+
+
 fn main() {
-    sys_open("hello.txt");
+    let mut src:[c_char;10]=[0;10];
+    src[0]='/' as c_char;
+
+    let mut target:[c_char;10]=[0;10];
+    target[0]='/' as c_char;
+    target[1]='m' as c_char;
+    target[2]='n' as c_char;
+    target[3]='t' as c_char;
+
+    let mut fstype:[c_char;10]=[0;10];
+    fstype[0]='r' as c_char;
+    fstype[1]='a' as c_char;
+    fstype[2]='m' as c_char;
+    fstype[3]='f' as c_char;
+    fstype[4]='s' as c_char;
+    unsafe{
+        let c_str=std::ffi::CStr::from_ptr(src.as_ptr());
+        println!("{:?}",c_str);
+    }
+    unsafe{
+        let result=mount(src.as_ptr(), target.as_ptr(), fstype.as_ptr(), 0, std::ptr::null());
+        if result!=0{
+            println!("mount fs failed!");
+        }
+    }
 }
